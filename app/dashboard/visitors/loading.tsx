acd54b9bed8b8e3a907a132183519e45a84361dc
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-07-16 00:19:07 +0200

    New: Enhance through adding loading page to visitors page

---
+++ app/dashboard/visitors/loading.tsx
@@ -0,0 +1,9 @@
+// DONE REVIEWING: GITHUB COMMIT
+
+import Loading from "../../../components/loading"
+
+const VisitorsLoadingPage = function VisitorsLoadingPage() {
+  return <Loading />
+}
+
+export default VisitorsLoadingPage
