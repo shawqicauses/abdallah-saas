c039473c4e38a60d61eafe317af319d81199d623
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-07-16 18:49:58 +0200

    New: Enhance through refactoring auth pages

---
+++ app/(auth)/loading.tsx
@@ -0,0 +1,9 @@
+// DONE REVIEWING: GITHUB COMMIT
+
+import Loading from "../../components/loading"
+
+const AuthenticationLoadingPage = function AuthenticationLoadingPage() {
+  return <Loading />
+}
+
+export default AuthenticationLoadingPage
