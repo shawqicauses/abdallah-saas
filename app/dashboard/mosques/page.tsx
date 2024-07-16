71f4edc04c3d693c438733d19ac953096162b4eb
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-07-16 20:34:54 +0200

    New: Enhance through refactoring mosques page

--- app/dashboard/mosques/page.tsx
+++ app/dashboard/mosques/page.tsx
@@ -1,101 +1,58 @@
-"use client"
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 
-// DONE REVIEWING: GITHUB COMMIT
-
-import {Mosque} from "@prisma/client"
-import {PlusCircleIcon} from "lucide-react"
+import {CopyPlusIcon, PlusCircleIcon} from "lucide-react"
 import Link from "next/link"
-import {Fragment, useEffect, useState, useTransition} from "react"
 import {
   Button,
   Card,
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
+  CardTitle
 } from "../../../components/ui"
-import {deleteMosque, getMosques} from "../../../server/actions/mosque"
+import {cn} from "../../../lib/utils"
+import {getMosques} from "../../../server/actions/mosque"
 
-const MosquesPage = function MosquesPage() {
-  const [isPending, startTransition] = useTransition()
-  const [mounted, setMounted] = useState(false)
-  const [refetch, setRefetch] = useState(new Date())
-  const [mosques, setMosques] = useState<Mosque[] | []>([])
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  useEffect(() => {
-    if (!mounted) return
-    startTransition(async () => {
-      const response = await getMosques()
-      if (response) setMosques(response)
-    })
-  }, [mounted, refetch])
-
-  if (!mounted) return null
-
+const MosquesPage = async function MosquesPage() {
+  const mosques = await getMosques()
   return (
-    <Fragment>
-      <div className="flex items-center">
-        <div className="ml-auto flex items-center gap-2">
-          <Button disabled={isPending} asChild>
-            <Link href="/dashboard/mosques/add">
-              <PlusCircleIcon className="mr-2 h-5 w-5" />
-              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Add Mosque</span>
-            </Link>
-          </Button>
-        </div>
-      </div>
-      <Card>
-        <CardHeader>
+    <Card className="flex flex-1 flex-col items-stretch justify-start">
+      <CardHeader className="flex-col gap-3 space-y-0 md:flex-row md:items-center md:justify-between">
+        <div className="space-y-1">
           <CardTitle>Mosques</CardTitle>
           <CardDescription>Check all mosques.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Name</TableHead>
-                <TableHead>
-                  <span className="sr-only">Actions</span>
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {mosques.map((mosque) => (
-                <TableRow key={mosque.id}>
-                  <TableCell className="font-medium">{mosque.name}</TableCell>
-                  <TableCell>
-                    <Button variant="accent" disabled={isPending} className="mb-2 sm:mr-2" asChild>
-                      <Link href={`/dashboard/mosques/edit/${mosque.id}`}>Edit</Link>
-                    </Button>
-                    <Button
-                      variant="outline"
-                      disabled={isPending}
-                      onClick={() => {
-                        startTransition(async () => {
-                          await deleteMosque(mosque.id)
-                          setRefetch(new Date())
-                        })
-                      }}>
-                      Delete
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
-    </Fragment>
+        </div>
+        <Button asChild>
+          <Link href="/dashboard/mosques/add">
+            <PlusCircleIcon className="mr-2 h-5 w-5" />
+            <span className="sm:whitespace-nowrap">Add Mosque</span>
+          </Link>
+        </Button>
+      </CardHeader>
+      <CardContent
+        className={cn(
+          "flex flex-1 justify-center",
+          mosques.length === 0 ? "items-center" : "items-stretch p-0"
+        )}>
+        {mosques.length === 0 ? (
+          <div className="w-fll flex h-full flex-col items-center justify-center">
+            <CopyPlusIcon strokeWidth={1.5} className="size-12 text-primary md:size-20" />
+            <h3 className="mt-4 text-xl font-semi-bold leading-none md:text-xl-2">No Mosques</h3>
+            <p className="mt-1 text-base leading-relaxed text-muted-foreground md:text-lg">
+              There are no mosques. Add one!
+            </p>
+            <Button className="mt-4" asChild>
+              <Link href="/dashboard/mosques/add">
+                <PlusCircleIcon className="mr-2 h-5 w-5" />
+                <span className="sm:whitespace-nowrap">Add Mosque</span>
+              </Link>
+            </Button>
+          </div>
+        ) : (
+          "Table"
+        )}
+      </CardContent>
+    </Card>
   )
 }
 
